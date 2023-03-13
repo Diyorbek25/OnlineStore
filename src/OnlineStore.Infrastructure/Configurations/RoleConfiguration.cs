ccd5e8e15046cc2be77e7a4590a5fac0bcc8d5f0
diyorbek2506@gmail.com <diyorbek2506@gmail.com>
2023-03-13 18:32:39 +0500

    Removed Admin Configuration and added Role Configuration

---
+++ src/OnlineStore.Infrastructure/Configurations/RoleConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OnlineStore.Domain.Entities;
+
+namespace OnlineStore.Infrastructure.Configurations;
+
+public class RoleConfiguration : IEntityTypeConfiguration<Role>
+{
+    public void Configure(EntityTypeBuilder<Role> builder)
+    {
+        builder.ToTable("Roles");
+
+        builder.HasKey(role => role.Id);
+
+        builder.Property(role => role.Name)
+            .IsRequired()
+            .HasMaxLength(30);
+    }
+}
