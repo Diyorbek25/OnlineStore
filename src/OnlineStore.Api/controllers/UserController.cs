25ecfd28f28c250a335d5db8e229c6a4aebc54d3
diyorbek2506@gmail.com <diyorbek2506@gmail.com>
2023-03-13 18:32:15 +0500

    added User controller

---
+++ src/OnlineStore.Api/controllers/UserController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineStore.Api.Controllers;
+
+[ApiController]
+[Route("api/User")]
+public class UserController : ControllerBase
+{
+    [HttpGet]
+    public ActionResult Get(int a)
+    {
+        return Ok();
+    }
+}
